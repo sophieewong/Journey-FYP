14b790bd15af18b367ea4f518359ad451294fb7b
sophieewong <48415531+sophieewong@users.noreply.github.com>
2019-11-24 15:35:07 +0000

    Rewrote Backend route

--- Backend/main.js
+++ Backend/main.js
@@ -1,13 +1,10 @@
 const path = require('path');
 
 const express = require("express");
-const app = express();
+const app = express(); //server
+
+// when browser requests route, server returns entire application to the browser
 app.use(express.static(path.resolve(__dirname, "../Frontend/dist/")))
-
-// browser requests route, server returns 
-app.get("/", (req, res) => {
-
-});
 
 app.listen(5000, () => console.log("server has started"));
 
